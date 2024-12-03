aabc971c0ba1afd8a7ca509ad5a39ac7983eca59
Cetin Erdem <cetoerdem1@gmail.com>
2024-12-03 20:54:38 +0400

    finalize typegen

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -13,7 +13,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
     _id,
     _createdAt,
     views,
-    author: { _id: authorId, name },
+    author,
     description,
     image,
     category,
@@ -30,14 +30,14 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       </div>
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
-            <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image
             src="https://placehold.co/48x48"
             alt="avatarplaceholder"
@@ -56,7 +56,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         />
       </Link>
       <div className="flex-between gap-3 mt-5">
-        <Link href={`/?query=${category.toLowerCase()}}`}>
+        <Link href={`/?query=${category?.toLowerCase()}}`}>
           <p className="text-16-medium">{category}</p>
         </Link>
         <Button className="startup_card_btn" asChild>
