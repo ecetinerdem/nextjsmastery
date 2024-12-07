c2ec339df87d2b67826b622710ef3d87dadce83c
Cetin Erdem <cetoerdem1@gmail.com>
2024-12-07 11:44:07 +0400

    Begin startup form

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import React from "react";
+import { Input } from "./ui/input";
 
 const StartupForm = () => {
-  return <div>StartupForm</div>;
+  return (
+    <form action={() => {}} className="startup-form">
+      <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          required
+          placeholder="Startup Title"
+        />
+      </div>
+    </form>
+  );
 };
 
 export default StartupForm;
