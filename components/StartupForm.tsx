68b1427c214df870788206a7415a0b90a58b3743
Cetin Erdem <cetoerdem1@gmail.com>
2024-12-08 22:50:16 +0400

    add Startup form

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 const StartupForm = () => {
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
   return (
     <form action={() => {}} className="startup-form">
       <div>
@@ -17,6 +20,63 @@ const StartupForm = () => {
           required
           placeholder="Startup Title"
         />
+        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+      </div>
+      <div>
+        <label htmlFor="description" className="startup-form_label">
+          Description
+        </label>
+        <Textarea
+          id="description"
+          name="description"
+          className="startup-form_textarea"
+          required
+          placeholder="Startup Description"
+        />
+        {errors.description && (
+          <p className="startup-form_error">{errors.description}</p>
+        )}
+      </div>
+      <div>
+        <label htmlFor="category" className="startup-form_label">
+          Category
+        </label>
+        <Input
+          id="category"
+          name="category"
+          className="startup-form_input"
+          required
+          placeholder="Startup Category (Tech, Health, Education ..."
+        />
+        {errors.category && (
+          <p className="startup-form_error">{errors.category}</p>
+        )}
+      </div>
+      <div>
+        <label htmlFor="link" className="startup-form_label">
+          Image URL
+        </label>
+        <Input
+          id="link"
+          name="link"
+          className="startup-form_input"
+          required
+          placeholder="Startup Image URL"
+        />
+        {errors.link && <p className="startup-form_error">{errors.link}</p>}
+      </div>
+      <div data-color-mode="light">
+        <label htmlFor="pitch" className="startup-form_label">
+          Pitch
+        </label>
+        <Textarea
+          id="pitch"
+          name="pitch"
+          className="startup-form_input"
+          required
+          placeholder="Startup Pitch"
+        />
+        {errors.pitch && <p className="startup-form_error">{errors.pitch}</p>}
       </div>
     </form>
   );
